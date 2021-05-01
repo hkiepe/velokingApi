8a43fb36f15b92e1ca2a8accc1a5d79763ebffc8
Henrik Kiepe <hkiepe@inkontor.com>
2021-05-01 11:01:34 +0200

    added flusk in main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restful import Api, Resource
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+app = Flask(__name__)
+api = Api(app)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+api.add_resource(HelloWorld, "/helloworld")
+
+if __name__ == "__main__":
+    app.run(debug=True)
